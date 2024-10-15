65b9476d19bae39b1184133138cbe5de7f81accf
Lucas Santos <161741045+LuzKS@users.noreply.github.com>
2024-10-15 11:10:18 -0300

    pg 12 - passo 7

---
+++ app/src/main/java/santos/souza/lucas/galeriapublica/MainViewModel.java
@@ -0,0 +1,21 @@
+package santos.souza.lucas.galeriapublica;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainViewModel extends AndroidViewModel {
+    int navigationOpSelected = R.id.gridViewOp;
+
+    public MainViewModel(@NonNull Application application){
+        super(application);
+    }
+    public int getNavigationOpSelected(){
+        return navigationOpSelected;
+    }
+
+    public void setNavigationOpSelected(int navigationOpSelected) {
+        this.navigationOpSelected = navigationOpSelected;
+    }
+}
